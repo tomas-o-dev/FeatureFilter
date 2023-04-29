1efaa3d2454beac4277c2b01a10da8dc9646fb27
tomas-o <todaniel649@gmail.com>
2023-04-29 12:45:03 +0800

    Add files via upload
    fixed 'sqrt' and 'log', added 'auto'

--- QLCFF/d_unif.py
+++ QLCFF/d_unif.py
@@ -23,7 +23,7 @@ class uniform(Discretizer):
         Get cutpoints from one continuous feature
         :param feature: continuous column to be discretized
         :param target: target column considered
-        :param mkbins: 'ten' [1],[2]; 'log' [1]; 'sqrt' [3]
+        :param mkbins: 'ten' [1],[2]; 'log' [1]; 'sqrt' [3], 'auto' [4]
 
     [1] J. Dougherty, R. Kohavi, and M. Sahami, “Supervised and unsupervised
     discretization of continuous features,” in ICML 1995, pp. 194–202
@@ -31,6 +31,7 @@ class uniform(Discretizer):
     for naive-bayes classifiers,” in Proc. PKAW 2002, pp. 159-173
     [3] Y. Yang and G. I. Webb, “Proportional k-interval discretization 
     for naive-bayes classifiers,” in Machine learning: ECML 2001, pp. 564–575
+    [4] https://numpy.org/doc/stable/reference/generated/numpy.histogram.html
 
         :return: List of cutpoints
         :        passthru feature_name
@@ -38,13 +39,26 @@ class uniform(Discretizer):
         n = len(np.unique(feature)) 
 
         if self.mkbins == 'sqrt':
-            nbins = round(np.sqrt(n))
+            nbins = floor(np.sqrt(n))
 
         elif self.mkbins == 'log':
-            nbins = max(2, 2*(round(np.log10(n))))
+            nbins = max(1, 2*(floor(np.log10(n))))
 
         else:
             nbins = 10
 
-        bin_edges = np.linspace(feature.min(), feature.max(), nbins + 1)
+        if self.mkbins == 'auto':
+            bnz, egz = np.histogram(feature, bins='auto')
+            egz = egz[np.nonzero(bnz)]
+
+            while (len(egz) /2) > 8:
+                egz[1::2] = [0]*(len(egz)//2)
+                egz = egz[np.nonzero(egz)]
+                egz = np.insert(egz, 0, min(feature))
+
+            bin_edges = np.append(egz,[feature.max()])
+
+        else:
+            bin_edges = np.linspace(feature.min(), feature.max(), nbins + 1)
+
         return feature_name, bin_edges[1:-1].tolist()
