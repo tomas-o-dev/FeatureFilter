e35f9a5906f65f27e524471fbb1cf5f7577e91c2
tomas-o <todaniel649@gmail.com>
2023-04-30 12:15:23 +0800

    Add files via upload
    signature changes for v005

--- QLCFF/__init__.py
+++ QLCFF/__init__.py
@@ -1,13 +1,13 @@
 
 from .d_mdlp import MDLP
 from .d_chim import ChiMerge
-from .d_unif import uniform
+from .d_nbhg import unifhgm
 
 from .f_fltr import filter_fcy, filter_fdr, filter_fcc
 from .f_rpts import get_filter, rpt_ycor, rpt_fcor
 
 __all__ = [
-    "MDLP", "ChiMerge", "uniform",
+    "MDLP", "ChiMerge", "unifhgm",
     "filter_fcy", "filter_fdr", "filter_fcc", 
     "get_filter", "rpt_ycor", "rpt_fcor"
 ]
