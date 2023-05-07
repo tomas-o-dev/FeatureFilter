1ec52557e33512fc1a79e3eb5af33bda053a2b3c
tomas-o <todaniel649@gmail.com>
2023-05-07 11:59:03 +0800

    Add files via upload
    rc0.1

--- QLCFF/c_dctzr.py
+++ QLCFF/c_dctzr.py
@@ -18,18 +18,17 @@ class Discretizer(BaseEstimator, TransformerMixin, metaclass=ABCMeta):
         self.msglvl=msglvl
 
 
-    def fit(self, X, y, detail=False):
+    def fit(self, X, y):
         '''
-        Get cutpoints, then discretize each feature
+        call the preprocessor
         :param X: pd.DataFrame with features
         :param y: pd.Series with target
-        :n_jobs, verbose: joblib
         :return: self
         '''
         from .d_ppdf import prep_df
-        from .d_nbhg import hgbins
 
-        prebin_df, col_list, lowuniq = prep_df(X)
+## log10(32) and sqrt(7) are first to give num_bins > 2
+        self.prebin_df, self.flist, self.vi, self.xxxi = prep_df(X)
 
         if isinstance(y, np.ndarray):
             sy = pd.Series(data=y, name='target')
@@ -39,46 +38,66 @@ class Discretizer(BaseEstimator, TransformerMixin, metaclass=ABCMeta):
         if sy.dtype == 'O':
             sy = pd.Series(data=LabelEncoder().fit_transform(sy),name=sy.name)
 
-        self.cutpoints = self._prep_get_cutpoints(prebin_df, sy, col_list)
+        self.sy = sy
 
-# log10(32) and sqrt(7) are first to give num_bins > 2
-# TODO with refactoring - uv < 32 for log; uv < 7 for sqrt, ten 
-        print('\nUnique Values < 32 - Always binned with =hgrm=')
-        for feature_name in lowuniq:
-            print('\t',feature_name,':: Unique Values =',len(prebin_df[feature_name].unique()))
-            feature = prebin_df[feature_name].values
-            bin_edges = hgbins(feature)
-            cutpoints = bin_edges[1:-1].tolist()
-            self.cutpoints.update({feature_name: cutpoints})
+        return self
 
-        self.binned_df = prebin_df.copy()
+
+    def transform(self, mkbins='unif-ten', detail=False):
+        '''
+        Get cutpoints, then discretize each feature
+        :detail: report outcome of binning
+        :return: discretized X
+        '''
+        from .d_nbhg import hgbins
+
+        if mkbins in ['unif-ten', 'unif-log', 'unif-sqrt', 
+                      'mdlp-ten', 'mdlp-log', 'mdlp-sqrt', 
+                      'chim-ten', 'chim-log', 'chim-sqrt']:
+            self.mkbins = mkbins
+        else:
+            self.mkbins='hgrm'
+
+## log10(32) and sqrt(7) are first to give num_bins > 2
+        col_list = [x for x in self.flist]
+        lowuniq = [x for x in self.vi]
+        if self.mkbins[5:] == 'log':
+            lowuniq.extend(x for x in self.xxxi)
+        else:
+            col_list.extend(x for x in self.xxxi)
+
+        self.cutpoints = self._prep_get_cutpoints(col_list)
+
+        if len(lowuniq):
+            print('\n Few Unique Values - Always binned with = hgrm =')
+            for feature_name in lowuniq:
+                print('\t',feature_name,':: Unique Values =',len(self.prebin_df[feature_name].unique()))
+                feature = self.prebin_df[feature_name].values
+                bin_edges = hgbins(feature)
+                cutpoints = bin_edges[1:-1].tolist()
+                self.cutpoints.update({feature_name: cutpoints})
+
+        self.binned_df = self.prebin_df.copy()
         for feature_name, cutpoints in self.cutpoints.items():
             if len(cutpoints) < 2:
                 print('\nWARNING: No',self.mkbins,'cutpoints could be calculated for',feature_name)
-                print('           Falling back to =hgrm=')
+                print('           Falling back to = hgrm =')
 
-                feature = X[feature_name].values
+                feature = self.prebin_df[feature_name].values
                 bin_edges = hgbins(feature)
                 cutpoints = bin_edges[1:-1].tolist()
 
             self.binned_df[feature_name] = pd.Series(
-                data=np.searchsorted(cutpoints, X.loc[:, feature_name]),
+                data=np.searchsorted(cutpoints, self.prebin_df.loc[:, feature_name]),
                 name=feature_name)
 
         self.binned_df.reset_index(inplace = True, drop = True)
-        return self
 
-
-    def transform(self, X, detail=False):
-        '''
-        :detail: report outcome of binning
-        :return: discretized X
-        '''
         if detail:
             colwid = max(len(n) for n in self.binned_df.columns.values)    # +i for padding
-            print('\nUnique value count: Original ::> Binned\n  Same for all features except:')
+            print('\nUnique value count: Original ::> Binned with',self.mkbins,'\n  Same for all features except:')
             for col in self.binned_df.columns:
-                lcx = len(X[col].unique())
+                lcx = len(self.prebin_df[col].unique())
                 lcb = len(self.binned_df[col].unique())
                 if lcx != lcb:
                     print("{: <{colwid}} {: >5} {} {: <5}".format(
@@ -86,30 +105,40 @@ class Discretizer(BaseEstimator, TransformerMixin, metaclass=ABCMeta):
 
         return self.binned_df
 
+##  --  --  --  --  ##
 
-    def fit_transform(self, X, y, detail=False):
-        return self.fit(X, y).transform(X, detail=detail)
+    def fit_transform(X, y, mkbins='unif-10', detail=False):
+        return self.fit(X, y).transform(mkbins, detail)
 
+##  --  --  --  --  ##
 
-    @abstractmethod
-    def _get_cutpoints(self):
-        '''
-        This method should describe how to get cutpoints according to each algorithm
-        '''
-        raise NotImplementedError
-
-
-    def _prep_get_cutpoints(self, features, target, col_list):
+    def _prep_get_cutpoints(self, col_list):
         '''
         Get cutpoints from every continuous feature
-        :param features, col_list: pd.Dataframe as col_list
-        :param target_: pd.Series with target
+        :features, col_list: pd.Dataframe as col_list
+        :target_: pd.Series with target
         :return: cutpoints for every continuous feature {feature name: List of cutpoints}
         '''
-        parallel = Parallel(n_jobs=self.numjobs, verbose=self.msglvl)
+        features = self.prebin_df
+        target = self.sy
+
+        numjobs = self.numjobs
+        msglvl = self.msglvl
+
+        if self.mkbins[:4] in ['unif', 'hgrm']:
+            from .d_nbhg import get_cutpoints
+            numjobs = 1
+            msglvl = 0
+        elif self.mkbins[:4] == 'chim':
+            from .d_chim import get_cutpoints
+        elif self.mkbins[:4] == 'mdlp':
+            from .d_mdlp import get_cutpoints
+
+        parallel = Parallel(n_jobs=numjobs, verbose=msglvl)
 
         work = parallel( 
-            delayed(self._get_cutpoints)(
+            delayed(get_cutpoints)(
+                self.mkbins[5:],
                 feature=features.loc[:, feature_name],
                 target=target,
                 feature_name=feature_name
