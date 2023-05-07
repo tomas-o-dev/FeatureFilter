336e37c6214e08ed4394cd859d0ba873fabc19fa
tomas-o <todaniel649@gmail.com>
2023-05-07 11:58:06 +0800

    Add files via upload
    rc0.1

--- QLCFF/__init__.py
+++ QLCFF/__init__.py
@@ -1,14 +1,18 @@
 
-from .d_mdlp import MDLP
-from .d_chim import ChiMerge
-from .d_nbhg import unifhgm
+#from .d_mdlp import MDLP
+#from .d_chim import ChiMerge
 
-from .f_fltr import filter_fcy, filter_fdr, filter_fcc
-from .f_rpts import get_filter, rpt_ycor, rpt_fcor
+from .c_dctzr import Discretizer
+from .c_ffltr import qlcfFilter
+
+#from .f_fltr import filter_fcy, filter_fdr, filter_fcc
+#from .f_rpts import get_filter, rpt_ycor, rpt_fcor
 
 __all__ = [
-    "MDLP", "ChiMerge", "unifhgm",
-    "filter_fcy", "filter_fdr", "filter_fcc", 
-    "get_filter", "rpt_ycor", "rpt_fcor"
+    "Discretizer", 'qlcfFilter'
 ]
+#    "filter_fcy", "filter_fdr", "filter_fcc", 
+#    "get_filter", "rpt_ycor", "rpt_fcor"
+#]
 
+# "MDLP", "ChiMerge",
